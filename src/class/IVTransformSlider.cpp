56a26c15aaaa6866277729837b79dafce57d7da4
NearGMD <9vhj7shs4v@privaterelay.appleid.com>
2026-09-05 23:28:45 +0200

    Update IVTransformSlider.cpp

--- src/class/IVTransformSlider.cpp
+++ src/class/IVTransformSlider.cpp
@@ -5,7 +5,8 @@ using namespace geode::prelude;
 
 GEODE_NS_IV_BEGIN
 
-TransformSlider::TransformSlider() {}
+TransformSlider::TransformSlider(LevelSettings& setting)
+    : m_currentSetting(setting) {}
 
 TransformSlider* TransformSlider::create(
     LevelSettings& setting,
@@ -14,7 +15,7 @@ TransformSlider* TransformSlider::create(
     char const* text,
     std::function<NodeTransform()>&& defaultPosFunc
 ) {
-    auto ret = new (std::nothrow) TransformSlider();
+    auto ret = new (std::nothrow) TransformSlider(setting);
 
     if (ret && ret->init(
         transform,
@@ -36,12 +37,22 @@ bool TransformSlider::init(
     char const* text,
     std::function<NodeTransform()>&& defaultPosFunc
 ) {
-    if (!CCLayer::init())
+    if (!CCNode::init())
         return false;
 
     m_transform = transform;
     m_inputNode = inputNode;
     m_defaultPosFunc = std::move(defaultPosFunc);
+
+    m_textLabel = CCLabelBMFont::create(
+        text ? text : "",
+        "goldFont.fnt"
+    );
+
+    if (m_textLabel) {
+        m_textLabel->setAnchorPoint({0.f, 0.5f});
+        this->addChild(m_textLabel);
+    }
 
     return true;
 }
@@ -52,6 +63,61 @@ void TransformSlider::setLevelSettings(LevelSettingsType type) {
             ? IVManager::get().m_settingClassic
             : IVManager::get().m_settingPlatformer
     );
+
+    updateSettingNodes();
+}
+
+void TransformSlider::onDefaultPosition(cocos2d::CCObject*) {
+    if (!m_defaultPosFunc)
+        return;
+
+    auto& setting = m_currentSetting.get().*m_transform;
+    setting = m_defaultPosFunc();
+
+    updateSettingNodes();
+}
+
+void TransformSlider::onSetVisibility(cocos2d::CCObject*) {
+    if (!m_visibilityCheckbox)
+        return;
+
+    auto& setting = m_currentSetting.get().*m_transform;
+
+    setting.isVisible = !setting.isVisible;
+
+    if (m_inputNode) {
+        m_inputNode->setVisible(setting.isVisible);
+    }
+}
+
+void TransformSlider::updateSettingNodes() {
+    if (!m_transform)
+        return;
+
+    auto& setting = m_currentSetting.get().*m_transform;
+
+    if (m_inputNode) {
+        m_inputNode->setPosition(setting.position);
+        m_inputNode->setScale(setting.scale);
+        m_inputNode->setRotation(setting.rotation);
+        m_inputNode->setVisible(setting.isVisible);
+    }
+
+    if (m_xPosSlider) {
+        m_xPosSlider->setValue(setting.position.x);
+    }
+
+    if (m_yPosSlider) {
+        m_yPosSlider->setValue(setting.position.y);
+    }
+
+    if (m_scaleSlider) {
+        m_scaleSlider->setValue(setting.scale);
+    }
+
+    if (m_visibilityCheckbox) {
+        m_visibilityCheckbox->toggle(setting.isVisible);
+    }
 }
 
 GEODE_NS_IV_END
