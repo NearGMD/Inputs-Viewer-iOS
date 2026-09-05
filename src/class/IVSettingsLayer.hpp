d4939bcfc8a841eeb78e50ccfdee491289e8aa10
NearGMD <9vhj7shs4v@privaterelay.appleid.com>
2026-09-05 23:45:26 +0200

    Update IVSettingsLayer.hpp

--- src/class/IVSettingsLayer.hpp
+++ src/class/IVSettingsLayer.hpp
@@ -1,33 +1,63 @@
 #pragma once
+
 #include "IVInputsViewLayer.hpp"
 #include "IVTransformSlider.hpp"
 #include "IVLevelSettings.hpp"
+#include "IVEvent.hpp"
+
+#include <Geode/ui/Popup.hpp>
 
 GEODE_NS_IV_BEGIN
 
-class SettingsLayer : public geode::Popup<bool> {
+class SettingsLayer : public geode::Popup {
 public:
     SettingsLayer(LevelSettingsType settingType);
-    static SettingsLayer* create(LevelSettingsType, bool enableGeodeSettingButton);
+
+    static SettingsLayer* create(
+        LevelSettingsType settingType,
+        bool enableGeodeSettingButton
+    );
+
 public:
     void onModSettings(cocos2d::CCObject*);
     void onClassic(cocos2d::CCObject*);
     void onPlatformer(cocos2d::CCObject*);
+
 public:
     void onExit() override;
+
 protected:
-    bool setup(bool enableGeodeSettingButton) override;
+    bool setup(
+        float width,
+        float height,
+        bool enableGeodeSettingButton
+    ) override;
+
     void setLevelSettings(LevelSettingsType type);
     void updateSettingNodes();
-    CCMenuItemToggler* createCheckbox(bool LevelSettings::* member, char const* text, std::optional<SettingEventType> postEvent, geode::Anchor anchor, cocos2d::CCPoint const& offset = {}, char const* description = nullptr);
+
+    CCMenuItemToggler* createCheckbox(
+        bool LevelSettings::* member,
+        char const* text,
+        std::optional<SettingEventType> postEvent,
+        geode::Anchor anchor,
+        cocos2d::CCPoint const& offset = {},
+        char const* description = nullptr
+    );
+
 protected:
     LevelSettingsType m_currentSettingType;
+
     std::reference_wrapper<LevelSettings> m_currentSetting;
+
     CCMenuItemSpriteExtra* m_classicBtn = nullptr;
     CCMenuItemSpriteExtra* m_platformerBtn = nullptr;
+
     InputsViewLayer* m_inputsLayer = nullptr;
+
     TransformSlider* m_p1Slider = nullptr;
     TransformSlider* m_p2Slider = nullptr;
+
     CCMenuItemToggler* m_totalInputsCheckbox = nullptr;
     CCMenuItemToggler* m_cpsCheckbox = nullptr;
     CCMenuItemToggler* m_hideLRCheckbox = nullptr;
